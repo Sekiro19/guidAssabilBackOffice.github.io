c0c89103ce783e2359b797a2663aced45759e679
Sekiro <126336555+Sekiro19@users.noreply.github.com>
2024-05-30 10:58:57 +0100

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './docs/**/*.html', './js/**/*.js'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
