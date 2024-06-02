3f3a4400ca2f4444f36478574cd8ebd62fb32b52
sekiro <aitmohammedabdelmalek19@gmail.com>
2024-06-02 10:47:18 +0100

    +

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./docs/**/*.{html,js}",
+    "./*.html",
+    "./js/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
